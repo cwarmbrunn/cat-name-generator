957f00c6fa92f44716cefce952ec5c4604b56ae9
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-03-17 09:29:41 -0400

    added dynamic voting buttons for food images and welcomes user with their input name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/*WELCOME DIV - Welcome/Form elements */
+var welcomeEl = document.getElementById("welcome");
+
 /* USER FORM - Declare User Form Variable */
 var userFormEl = document.getElementById("user-form");
 
@@ -7,8 +10,14 @@ var nameInputEl = document.querySelector(".form-input");
 /* LOCAL STORAGE - Setting Up localStorage to grab name */
 let userNameInfo = JSON.parse(localStorage.getItem(name)) || [];
 
-/* FOOD IMAGE LOCATION - Area where food images will load */
-var foodContentEl = document.getElementById("food-content");
+/* FOOD IMAGE DIV - Area where food images will load */
+var foodTitleEl = document.getElementById("food-title");
+
+/* FOOD IMAGES - P element for food images */
+var foodPicsEl = document.getElementById("foodpics");
+
+/* FOOD VOTING BUTTONS - Buttons to vote on food image */
+var foodVotingEl = document.getElementById("buttons");
 
 // Function that starts when submit button is clicked
 var startFunction = function (event) {
@@ -28,15 +37,40 @@ var startFunction = function (event) {
 
 // Foodish Image Generator
 var foodImageGenerator = function () {
+  hide(welcomeEl);
   // Test that function is working with an alert
   var foodishURL = "https://foodish-api.herokuapp.com/";
-
+  var name = nameInputEl.value.trim();
   console.log(foodishURL);
   fetch(foodishURL).then(function (response) {
     if (response.ok) {
-      var foodPic = document.createElement("img");
-      foodPic.setAttribute("src", foodishURL);
+      // Adds in dynamic HTML that addresses the user by their name
+      foodTitleEl.innerHTML =
+        "<h3> Hi there " +
+        name +
+        "," +
+        " what do you think about this dish? </h3> ";
 
+      //Adds in dynamic HTML that shows a random food image
+      foodPicsEl.innerHTML = "<img src= " + foodishURL + ">";
+
+      //   var foodPic = document.createElement("img");
+      //   foodPic.setAttribute("src", foodishURL);
+
+      // Adds in a "yes" or "no" voting button for the food image
+      foodVotingEl.innerHTML =
+        "<button class = 'btn' id= 'yesBtn'> Like It! </button>" +
+        "<button class = 'btn' id='noBtn'> Dislike It! </button>";
+
+      /* FOOD VOTING BUTTONS (YES) - Yes button to vote on food image */
+      var voteYesEl = document.getElementById("yesBtn");
+      /* FOOD VOTING BUTTONS (NO)  - No button to vote on food image */
+      var voteNoEl = document.getElementById("noBtn");
+      
+      // When user clicks the yes button under the food image - run the following function
+      voteYesEl.addEventListener("click", console.log("You clicked yes!"));
+      // When the user clicks on the no button under the food image - run the following function
+      voteNoEl.addEventListener("click", console.log("You clicked no!"));
       console.log("All good!");
     } else {
       console.log("Help!");
@@ -49,7 +83,7 @@ var userName = {
   userInput: name,
 };
 userNameInfo.push(userName);
-localStorage.setItem("name", JSON.stringify(userNameInfo));
+localStorage.setItem("username", JSON.stringify(userNameInfo));
 
 // Hide Elements
 function hide(element) {
